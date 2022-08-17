67249542d31c289c2e1a542f2a1dd425893eefc6
caglaozbb <caglaozbb@gmail.com>
2022-08-17 17:02:29 +0300

    Orders sayfası güncellendi

--- my-app/src/Orders.js
+++ my-app/src/Orders.js
@@ -8,6 +8,8 @@ export const Orders = () => {
 
     const navigate = useNavigate();
 
+    const [rows, setRows] = useState([]);
+
     //cıkısyap
     function logOut() {
         window.sessionStorage.clear();
@@ -15,10 +17,132 @@ export const Orders = () => {
         window.location.reload();
     }
 
-     ////modal siparis olustur////////
+    //Order ekle//
+    const [CarID,setCarID] = useState();
+    const [CarModel,setCarModel] = useState("");
+    const [CarColor,setCarColor] = useState("");
+    const [CarFuelType,setCarFuelType] = useState("");
+    const [CustomerID,setCustomerID] = useState();
+    const [CarDate,setCarDate] = useState();
+
+
+    ////modal siparis ekle////////
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    // ////modal siparis düzenle////////
+    // const [show2, setShow2] = useState(false);
+    // const handleClose2 = () => setShow2(false);
+    // const handleShow2 = () => setShow2(true);
+
+    useEffect(() => {
+      let xmls = stringInject(
+        '<?xml version="1.0" encoding="utf-8"?>'+
+          '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
+            '<soap:Body>'+
+              '<Getcars xmlns="http://tempuri.org/" />'+
+            '</soap:Body>'+
+          '</soap:Envelope>',
+        {}
+      );
+      axios
+      .post("https://localhost:44329/WebService1.asmx?WSDL", xmls, {
+        headers: {
+          "Content-Type": "text/xml",
+        },
+      })
+      .then((res) => {
+        var resStr = JSON.stringify(res.data);
+        var XMLParser = require("react-xml-parser");
+        var xml = new XMLParser().parseFromString(resStr);
+        var carIDArray = xml.getElementsByTagName("carID");
+        var carModelArray = xml.getElementsByTagName("carModel");
+        var carColorArray = xml.getElementsByTagName("carColor");
+        var carFuelTypeArray = xml.getElementsByTagName("carFuelType");
+        var customerIDArray = xml.getElementsByTagName("customerID");
+        var carDateArray = xml.getElementsByTagName("carDate");
+console.log("test");
+console.log(carModelArray.length);
+        console.log(carIDArray[0].value);
+        console.log(carModelArray[0].value);
+        console.log(carColorArray[0].value);
+        console.log(carFuelTypeArray[0].value);
+        console.log(customerIDArray[0].value);
+        console.log(carDateArray[0].value);
+
+        setRows([]);
+        for (let ind = 0; ind < carModelArray.length; ind++) {
+          console.log(ind);
+          setRows((prewRows) => [
+            ...prewRows,
+
+              <tr>
+                <td>{carIDArray[ind].value}</td>
+                <td>{carModelArray[ind].value}</td>
+                <td>{carColorArray[ind].value}</td>
+                <td>{carFuelTypeArray[ind].value}</td>
+                <td>{customerIDArray[ind].value}</td>
+                <td>{carDateArray[ind].value}</td>
+                <td>
+                <Button variant="danger">Delete</Button> 
+                </td>
+                <td>
+                </td>
+              </tr>,
+            ]);
+        } 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }, []);
+
+    //orderekle fonksiyonu
+    function addOrder(){
+      var CarModel2 = CarModel
+      var CarColor2 = CarColor
+      var CarFuelType2 = CarFuelType
+      var CustomerID2 = CustomerID
+      var CarDate2 = CarDate
+      let xmls = stringInject(
+        '<?xml version="1.0" encoding="utf-8"?>'+
+          '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'+
+            '<soap:Body>'+
+              '<AddCar xmlns="http://tempuri.org/">'+
+                '<CarModel>{CarModel2}</CarModel>'+
+                '<CarColor>{CarColor2}</CarColor>'+
+                '<CarFuelType>{CarFuelType2}</CarFuelType>'+
+                '<CustomerID>{CustomerID2}</CustomerID>'+
+                '<CarDate>{CarDate2}</CarDate>'+
+              '</AddCar>'+
+            '</soap:Body>'+
+          '</soap:Envelope>',
+      {CarModel2: CarModel2, CarColor2: CarColor2, CarFuelType2: CarFuelType2, CustomerID2: CustomerID2, CarDate2: CarDate2}
+      );
+      console.log(xmls)
+      axios
+      .post("https://localhost:44329/WebService1.asmx?WSDL", xmls,{
+        headers:{
+          "Content-Type": "text/xml",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        // console.log("Test");
+        var resStr = JSON.stringify(res.data);
+        var XMLParser = require("react-xml-parser");
+        var xml = new XMLParser().parseFromString(resStr);
+        var authArray = xml.getElementsByTagName("AddCarResult");
+        console.log(authArray);
+        var auth = authArray[0].value;
+        console.log(auth);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+
 
     return(
         <Container>
@@ -32,7 +156,7 @@ export const Orders = () => {
                   <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                       <Nav.Link href="/home">Home</Nav.Link>
-                      <Nav.Link href="#link">Orders</Nav.Link>
+                      <Nav.Link href="/orders">Orders</Nav.Link>
                       <NavDropdown title="Actions" id="basic-nav-dropdown">
                         <NavDropdown.Item href="/UserList" >
                           UserList
@@ -58,21 +182,23 @@ export const Orders = () => {
               <table className="table table-striped">
                 <thead>
                   <tr>
+                    <th>ID</th>
                     <th>Model</th>
                     <th>Color</th>
                     <th>Fuel Type</th>
+                    <th>Customer ID</th>
                     <th>Delivery Date</th>
                     <th>
-                      {/* <Button onClick={handleShow}>Add</Button> */}
+                      <Button onClick={handleShow}>Add</Button>
                     </th>
                     <th>
-                {/* <Button variant="secondary" onClick={handleShow2}>Update</Button> */}
+                 <Button variant="secondary">Update</Button> 
                     </th>
                   </tr>
                 </thead>
-                {/* {rows.map((entry) => (
-                  <tbody>{entry}</tbody>
-                ))} */}
+                 {rows.map( (entry) => (
+                    <tbody>{entry}</tbody>
+                  ))} 
               </table>
             </div>
             
@@ -80,22 +206,24 @@ export const Orders = () => {
         <Modal.Header closeButton>
           <Modal.Title>Add a new Order</Modal.Title>
         </Modal.Header>
-        {/* <Modal.Body>  
+         <Modal.Body>  
           <input type="text" className="form-control" placeholder="Car model" onChange={e=>setCarModel(e.target.value)}/>
           <br></br>
           <input type="text" className="form-control" placeholder="Car Color" onChange={e=>setCarColor(e.target.value)}/>
           <br></br>
           <input type="text" className="form-control" placeholder="Car Fuel Type" onChange={e=>setCarFuelType(e.target.value)}/>
           <br></br>
+          <input type="text" className="form-control" placeholder="Customer ID" onChange={e=>setCustomerID(e.target.value)}/> 
+          <br></br>
           <input type="text" className="form-control" placeholder="Car Delivery Date" onChange={e=>setCarDate(e.target.value)}/> 
-        </Modal.Body> */}
+        </Modal.Body> 
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          {/* <Button variant="primary" onClick={addOrder}>
+          <Button variant="primary" onClick={addOrder}>
             Save Changes
-          </Button> */}
+          </Button> 
         </Modal.Footer>
       </Modal>
 {/* 
